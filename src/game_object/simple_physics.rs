11c21616b26971d0cff551bea978ed2c70e2f725
lcdr <lcdr@users.noreply.github.com>
2021-03-30 23:43:52 +0200

    Implement rotation for simple physics

--- src/game_object/simple_physics.rs
+++ src/game_object/simple_physics.rs
@@ -4,22 +4,30 @@ use lu_packets::{
 	world::{LuNameValue, LnvValue, Quaternion, Vector3},
 };
 
+use crate::services::GameObjectService;
 use super::InternalComponent;
 
 pub struct SimplePhysicsComponent {
 	position: Vector3,
+	rotation: Quaternion,
 }
 
 impl InternalComponent for SimplePhysicsComponent {
 	type ComponentProtocol = SimplePhysicsProtocol;
 
 	fn new(config: &LuNameValue) -> Self {
-		let x = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_x")) { *x } else { 156.0 };
-		let y = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_y")) { *x } else { 380.0 };
-		let z = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_z")) { *x } else { -187.0 };
+		let pos_x = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_x")) { *x } else { 156.0 };
+		let pos_y = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_y")) { *x } else { 380.0 };
+		let pos_z = if let Some(LnvValue::F32(x)) = config.get(&lu!("position_z")) { *x } else { -187.0 };
+
+		let rot_x = if let Some(LnvValue::F32(x)) = config.get(&lu!("rotation_x")) { *x } else { 0.0 };
+		let rot_y = if let Some(LnvValue::F32(x)) = config.get(&lu!("rotation_y")) { *x } else { 0.0 };
+		let rot_z = if let Some(LnvValue::F32(x)) = config.get(&lu!("rotation_z")) { *x } else { 0.0 };
+		let rot_w = if let Some(LnvValue::F32(x)) = config.get(&lu!("rotation_w")) { *x } else { 0.0 };
 
 		Self {
-			position: Vector3 { x, y, z }
+			position: Vector3 { x: pos_x, y: pos_y, z: pos_z },
+			rotation: Quaternion { x: rot_x, y: rot_y, z: rot_z, w: rot_w },
 		}
 	}
 
@@ -30,12 +38,7 @@ impl InternalComponent for SimplePhysicsComponent {
 			motion_type: None,
 			position_rotation_info: Some(PositionRotationInfo {
 				position: self.position,
-				rotation: Quaternion {
-					x: 0.0,
-					y: 0.7334349751472473,
-					z: 0.0,
-					w: 0.6797596216201782,
-				},
+				rotation: self.rotation,
 			}),
 		}
 	}
@@ -45,6 +48,18 @@ impl InternalComponent for SimplePhysicsComponent {
 			velocity_info: None,
 			motion_type: None,
 			position_rotation_info: None,
+		}
+	}
+
+	fn run_service(&self, service: &mut GameObjectService) {
+		match service {
+			GameObjectService::GetPosition(x) => {
+				x.0 = self.position;
+			}
+			GameObjectService::GetRotation(x) => {
+				x.0 = self.rotation;
+			}
+			_ => {},
 		}
 	}
 }
